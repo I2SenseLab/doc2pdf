ed2167e5d586e4807fe7b1d7a3e89ff5dd5b1b57
Devin <devin@atkin.engineer>
2022-08-08 15:12:11 -0600

    updated flask launch stuff

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route("/certificate")
 def certificateHandler():
     print(request)
+    return request
+
+
+if __name__ == "__main__":
+    content = ("MDAcert.pem","MDAkey.pem")
+    app.run(ssl_context=content,debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
