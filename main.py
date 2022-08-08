d84360f0af086d1406f9fa96de0e3cdeadef6f10
Devin <devin@atkin.engineer>
2022-08-08 15:12:11 -0600

    updated flask launch stuff

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-from flask import Flask, request
-
+from flask import Flask, request, send_file
+from documentCreation import convert_to
+import os 
+import tempfile
 
 app = Flask(__name__)
 app.secret_key = 'thereisacatinthemiddleoftown'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route("/certificate")
-def certificateHandler():
-    print(request)
+@app.route("/convert_doc_to_pdf", methods=['POST'])
+def convert_doc_to_pdf():
+    print("Converting Document To PDF")
+
+    if request.method == 'POST':
+        #Create a temporary file for the document
+        file_name = next(tempfile._get_candidate_names())
+        file_path = os.path.join(tempfile.gettempdir(), file_name)
+
+        f= request.files['file']
+        f.save(file_path)
+
+
+        try:
+            file_path = convert_to(tempfile.gettempdir(),file_path)
+        except:
+            file_path = "BADPDF.pdf"
+
+        response = send_file(
+            file_path,
+            mimetype='image/png',
+            as_attachment=True,
+            download_name='file.pdf'
+        )
+
+        return response
+    else:
+        return "Not a post request"
+
+@app.route("/")
+def running():
+
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    content = ("MDAcert.pem","MDAkey.pem")
+    app.run(ssl_context=content,debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
