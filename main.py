041f7d77551aee848e567048e84d50232c09981d
Devin <devinatkin@gmail.com>
2023-01-03 15:43:09 -0700

    gone back to hello world

--- main.py
+++ main.py
@@ -4,37 +4,35 @@ import os
 import tempfile
 
 app = Flask(__name__)
-app.secret_key = 'thereisacatinthemiddleoftown'
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-@app.route("/convert_doc_to_pdf", methods=['POST'])
-def convert_doc_to_pdf():
-    print("Converting Document To PDF")
+# @app.route("/convert_doc_to_pdf", methods=['POST'])
+# def convert_doc_to_pdf():
+#     print("Converting Document To PDF")
 
-    if request.method == 'POST':
-        #Create a temporary file for the document
-        file_name = next(tempfile._get_candidate_names())
-        file_path = os.path.join(tempfile.gettempdir(), file_name)
+#     if request.method == 'POST':
+#         #Create a temporary file for the document
+#         file_name = next(tempfile._get_candidate_names())
+#         file_path = os.path.join(tempfile.gettempdir(), file_name)
 
-        f= request.files['file']
-        f.save(file_path)
+#         f= request.files['file']
+#         f.save(file_path)
 
 
-        try:
-            file_path = convert_to(tempfile.gettempdir(),file_path)
-        except:
-            file_path = "BADPDF.pdf"
+#         try:
+#             file_path = convert_to(tempfile.gettempdir(),file_path)
+#         except:
+#             file_path = "BADPDF.pdf"
 
-        response = send_file(
-            file_path,
-            mimetype='image/png',
-            as_attachment=True,
-            download_name='file.pdf'
-        )
+#         response = send_file(
+#             file_path,
+#             mimetype='image/png',
+#             as_attachment=True,
+#             download_name='file.pdf'
+#         )
 
-        return response
-    else:
-        return "Not a post request"
+#         return response
+#     else:
+#         return "Not a post request"
 
 @app.route("/")
 def running():
@@ -44,5 +42,4 @@ def running():
 
 
 if __name__ == "__main__":
-    content = ("MDAcert.pem","MDAkey.pem")
-    app.run(ssl_context=content,debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
